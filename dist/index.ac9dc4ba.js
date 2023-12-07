c3b3b5d84263bfcce510249e32c7d44eab24978c
CodeSandbox <support@codesandbox.io>
2023-12-07 10:10:28 +0000

    Initial commit

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -576,9 +576,31 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"aR1JP":[function(require,module,exports) {
 var _stylesCss = require("./styles.css");
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+const url = [
+    "https://fakestoreapi.com/products",
+    "https://fakestoreapi.com/carts"
+];
+const responseStatus = (response)=>{
+    if (response?.status >= 200 && response.status < 300) return Promise.resolve(response);
+    else return Promise.reject(new Error(response?.statusText || "Something went wrong"));
+};
+const json = (response)=>response.json();
+const handleError = (error)=>console.error(error);
+const fetchData = async (url)=>{
+    return fetch(url).then(responseStatus).then(json).catch(handleError);
+};
+const promises = url.map((endpoint)=>fetchData(endpoint));
+const displayCartItem = (data, element = "")=>{
+    const container = document.getElementById(element);
+    const preItems = (data || [])?.map((item)=>`
+        <pre>${JSON.stringify(item, null, 8)}</pre>
+    `).join("");
+    container.innerHTML = preItems;
+};
+Promise.all(promises).then(([products, carts])=>{
+    displayCartItem(products, "products");
+    displayCartItem(carts, "carts");
+});
 
 },{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
